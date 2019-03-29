15741b41d2d5987322e913ab820fdbbf785128df
yaninfo163 <yaninfo@126.com>
2019-03-29 17:48:58 +0800

    music list change style

--- EPlayer/app/src/main/java/com/example/eplayer/view/adapter/MusicAdapter.java
+++ EPlayer/app/src/main/java/com/example/eplayer/view/adapter/MusicAdapter.java
@@ -5,11 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.example.eplayer.R;
 import com.example.eplayer.entity.Music;
-
 import java.text.SimpleDateFormat;
 import java.util.List;
 
@@ -26,16 +25,14 @@ public class MusicAdapter extends ArrayAdapter<Music> {
         // 获取当前项的Music实例
         Music music = getItem(position);
         View view = LayoutInflater.from(getContext()).inflate(resourceId, null);
-        TextView musicId = view.findViewById(R.id.music_id);
         TextView musicTitle = view.findViewById(R.id.music_title);
-        TextView musicUrl = view.findViewById(R.id.music_url);
         TextView musicAuthor = view.findViewById(R.id.music_author);
         TextView musicDuration = view.findViewById(R.id.music_duration);
-        musicId.setText("" + music.getId());
+        ImageView imageList = view.findViewById(R.id.image_list);
         musicTitle.setText(music.getTitle());
-        musicUrl.setText(music.getUrl());
         musicAuthor.setText("" + music.getAuthor());
         musicDuration.setText(new SimpleDateFormat("mm:ss").format(music.getDuration()));
+        imageList.setImageBitmap(music.getMusicBitMap());
         return view;
     }
 
